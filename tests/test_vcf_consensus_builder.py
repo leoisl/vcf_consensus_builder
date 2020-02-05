50ffcf7912c274c6595a4215dacb67d0c3a00d84
leandro <leandro@ebi.ac.uk>
2020-02-05 20:23:14 +0000

    Updating tests
    TODO: replace these end-to-end tests by smaller unit tests with mocking... it will be hardly done though...

--- tests/test_vcf_consensus_builder.py
+++ tests/test_vcf_consensus_builder.py
@@ -11,84 +11,71 @@ from click.testing import CliRunner
 
 import cli
 
-VCF_DEL = 'tests/data/test.vcf'
-VCF_INS = 'tests/data/test2.vcf'
+VCF = 'tests/data/test.vcf'
 REF_FASTA = 'tests/data/ref.fa'
 DEPTHS = 'tests/data/test-depths.tsv'
 OUTPUT_FASTA = 'out.fa'
 
 
-def test_command_line_interface():
+def test_command_line_interface_help():
     """Test the CLI."""
     runner = CliRunner()
-
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert re.search(r'--help\s+Show this message and exit.', help_result.output, flags=re.DOTALL) is not None
 
+def test_command_line_interface_vcf_and_ref():
     # Test replacing multi-char deletion and SNPs and setting sample name via command-line
+    runner = CliRunner()
     with TemporaryDirectory(prefix='vcf_consensus_builder', dir='/tmp') as tempdir:
         temppath = Path(tempdir)
         full_fasta_output = temppath / OUTPUT_FASTA
-        sample_name = 'SAMPLE1'
         result = runner.invoke(cli.main,
-                               ['-v', VCF_DEL,
+                               ['-v', VCF,
                                 '-d', DEPTHS,
                                 '-r', REF_FASTA,
-                                '-o', full_fasta_output,
-                                '--sample-name', sample_name])
+                                '-o', full_fasta_output])
         assert result.exit_code == 0
-        rec = SeqIO.read(full_fasta_output, 'fasta')
-        assert str(rec.seq) == 'NACCGTANACAATAN--', 'There must be a deletion of 3 characters in the middle of the seq'
-        assert rec.id == sample_name
+        records = list(SeqIO.parse(full_fasta_output, 'fasta'))
+        assert str(records[0].seq) == 'NACCGTANACAATAN--'
+        assert str(records[1].seq) == 'TATACACATCCACGGC-N-N-N-N-N--T'
+
+def test_command_line_interface_no_coverage_threshold():
+    # Test replacing multi-char deletion and SNPs and setting sample name via command-line
+    runner = CliRunner()
 
     # Test changing no coverage threshold
     with TemporaryDirectory(prefix='vcf_consensus_builder', dir='/tmp') as tempdir:
         temppath = Path(tempdir)
         full_fasta_output = temppath / OUTPUT_FASTA
-        sample_name = 'SAMPLE1'
         result = runner.invoke(cli.main,
-                               ['-v', VCF_DEL,
+                               ['-v', VCF,
                                 '-d', DEPTHS,
                                 '-r', REF_FASTA,
                                 '-o', full_fasta_output,
-                                '--sample-name', sample_name,
-                                '--no-coverage', 4])
+                                '--no-coverage', 5])
         assert result.exit_code == 0
-        rec = SeqIO.read(full_fasta_output, 'fasta')
-        assert str(rec.seq) == '-ACCGTA-ACAATA---', 'Positions below 5X coverage must be replaced with "-"'
-        assert rec.id == sample_name
+        records = list(SeqIO.parse(full_fasta_output, 'fasta'))
+        assert str(records[0].seq) == '-ACCGTA-ACAAT----', 'Positions <= 5X coverage must be replaced with "-"'
+        assert str(records[1].seq) == 'TATACACATCCACG---------------', 'Positions <= 5X coverage must be replaced with "-"'
 
+def test_command_line_interface_low_and_no_coverage_threshold_with_other_chars():
     # Test replacing low and no coverage characters with other characters than default N and - respectively
+    runner = CliRunner()
+
     with TemporaryDirectory(prefix='vcf_consensus_builder', dir='/tmp') as tempdir:
         temppath = Path(tempdir)
         full_fasta_output = temppath / OUTPUT_FASTA
-        sample_name = 'SAMPLE1'
         result = runner.invoke(cli.main,
-                               ['-v', VCF_DEL,
+                               ['-v', VCF,
                                 '-d', DEPTHS,
                                 '-r', REF_FASTA,
                                 '-o', full_fasta_output,
-                                '--sample-name', sample_name,
                                 '--no-cov-char', '=',
                                 '--low-cov-char', '@'])
         assert result.exit_code == 0
-        rec = SeqIO.read(full_fasta_output, 'fasta')
-        assert str(rec.seq) == '@ACCGTA@ACAATA@==', \
+        records = list(SeqIO.parse(full_fasta_output, 'fasta'))
+        assert str(records[0].seq) == '@ACCGTA@ACAATA@==', \
             'No coverage positions must be replaced with "=". Low coverage (<5X) positions must be replaced with "@".'
-        assert rec.id == sample_name
-
-    # Test replacing multi-char insertion and SNPs
-    with TemporaryDirectory(prefix='vcf_consensus_builder', dir='/tmp') as tempdir:
-        temppath = Path(tempdir)
-        full_fasta_output = temppath / OUTPUT_FASTA
-        result = runner.invoke(cli.main,
-                               ['-v', VCF_INS,
-                                '-d', DEPTHS,
-                                '-r', REF_FASTA,
-                                '-o', full_fasta_output])
-        assert result.exit_code == 0
-        rec = SeqIO.read(full_fasta_output, 'fasta')
-        assert str(rec.seq) == 'NACCGTATTTGTCNACAATAN--', 'There must be an insertion of "TTT" in the middle of the seq'
-        assert rec.id == 'sample1', \
-            'FASTA ID must be the first sample name in the VCF if not explicitly specified as a command-line arg'
+        assert str(records[1].seq) == 'TATACACATCCACGGC=@=@=@=@=@==T', \
+            'No coverage positions must be replaced with "=". Low coverage (<5X) positions must be replaced with "@".'
